8805d1b46f0d1f5f000933a2ff2240bbe206c20c
anton_isachenka <as005409@g.bstu.by>
2021-06-27 19:32:48 +0300

    [fix] add template of some action and some controller

--- src/main/java/com/foxrider/actions/TranslateAction.java
+++ src/main/java/com/foxrider/actions/TranslateAction.java
@@ -5,21 +5,21 @@ import com.intellij.openapi.actionSystem.AnActionEvent;
 import com.intellij.openapi.actionSystem.PlatformDataKeys;
 import com.intellij.openapi.editor.Editor;
 import com.intellij.openapi.ui.Messages;
+import org.apache.commons.lang.StringUtils;
 import org.jetbrains.annotations.NotNull;
-
-import java.net.URLEncoder;
-import java.nio.charset.StandardCharsets;
 
 public class TranslateAction extends AnAction {
     @Override
     public void actionPerformed(@NotNull AnActionEvent event) {
-        Messages.showMessageDialog("hello", "AITranslate+",Messages.getInformationIcon());
+
         Editor editor = event.getData(PlatformDataKeys.EDITOR);
         String selectedText = editor.getSelectionModel().getSelectedText();
 //        String encode = URLEncoder.encode(selectedText, StandardCharsets.UTF_8);
-
-         // сделать запрос какому-нибудь сервису переводов с параметрами определить язык с переводом на заданный в настройках язык
-         // отобразить ответ сервиса в окошке возможно даже взять из оригинального AITranslate
+        if(StringUtils.isNotEmpty(selectedText)){
+            Messages.showMessageDialog(selectedText, "AITranslate+", Messages.getInformationIcon());
+        }
+        // сделать запрос какому-нибудь сервису переводов с параметрами определить язык с переводом на заданный в настройках язык
+        // отобразить ответ сервиса в окошке возможно даже взять из оригинального AITranslate
     }
 
     @Override
--- src/main/java/com/foxrider/controller/TranslateController.java
+++ src/main/java/com/foxrider/controller/TranslateController.java
@@ -1,4 +1,16 @@
 package com.foxrider.controller;
 
+import com.detectlanguage.DetectLanguage;
+import com.detectlanguage.Result;
+import com.detectlanguage.errors.APIError;
+import com.foxrider.model.ReverseContextResponse;
+
+import java.util.List;
+
 public class TranslateController {
+    public ReverseContextResponse postForObject() throws APIError {
+
+        // do post request to reverse context and return model
+        return null;
+    }
 }
