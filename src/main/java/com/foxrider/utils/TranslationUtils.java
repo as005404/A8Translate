04ba66965b0afad4a180aad4e1f0c7234b98c52f
anton_isachenka <as005409@g.bstu.by>
2021-06-29 23:23:44 +0300

    [fix] change colors in baloon

--- src/main/java/com/foxrider/utils/TranslationUtils.java
+++ src/main/java/com/foxrider/utils/TranslationUtils.java
@@ -3,16 +3,16 @@ package com.foxrider.utils;
 import com.intellij.openapi.editor.Editor;
 import com.intellij.openapi.ui.popup.Balloon;
 import com.intellij.openapi.ui.popup.JBPopupFactory;
+import com.intellij.ui.Gray;
 import com.intellij.ui.JBColor;
 
 import java.awt.*;
 
 public class TranslationUtils {
-// todo: implement ability t ochange colors to distinct different cases
-    //todo: mby change colors
+
     public static void showPopupWindow(Editor editor, String result) {
         JBPopupFactory.getInstance()
-                .createHtmlTextBalloonBuilder(result, null, new JBColor(new Color(238, 172, 62), new Color(73, 117, 73)), null)
+                .createHtmlTextBalloonBuilder(result, null, new JBColor(new Color(255, 192, 203), Gray._68), null)
                 .setFadeoutTime(15000)
                 .setHideOnAction(true)
                 .createBalloon()
