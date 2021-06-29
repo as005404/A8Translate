b361567db48eb2bcf35980fc78aff87ef37b3abe
anton_isachenka <as005409@g.bstu.by>
2021-06-29 23:26:02 +0300

    [fix] change parser to expect situation when results are empty

--- src/main/java/com/foxrider/parser/ContextReverseResponseParser.java
+++ src/main/java/com/foxrider/parser/ContextReverseResponseParser.java
@@ -1,5 +1,6 @@
 package com.foxrider.parser;
 
+import com.foxrider.model.response.ContextResults;
 import com.foxrider.model.response.ContextReverseResponse;
 import com.foxrider.model.response.Result;
 import com.google.common.base.Strings;
@@ -11,19 +12,23 @@ import java.util.stream.Collectors;
 // todo: make camelCase into normal text
 // todo: make reverse translation from one prefference language to another(core language)
 public class ContextReverseResponseParser implements ResponseParser {
-    private final static Integer TRANSLATION_LIMIT = 7;
+    private final static Integer TRANSLATION_LIMIT = 5;
+
     @Override
     public String parse(ContextReverseResponse response) {
         StringBuilder builder = new StringBuilder();
         List<String> translate = new ArrayList<>(response.getTranslation());
 
-        List<String> fromResults = response.getContextResults().getResults()
-                .stream()
-                .map(Result::getTranslation)
-                .collect(Collectors.toList());
+        //todo: results may be empty
+        ContextResults results = response.getContextResults();
+        if (results != null && results.getResults().size() != 0) {
+            List<String> fromResults = results.getResults()
+                    .stream()
+                    .map(Result::getTranslation)
+                    .collect(Collectors.toList());
 
-        translate.addAll(fromResults);
-
+            translate.addAll(fromResults);
+        }
         translate.stream()
                 .map(String::toLowerCase)
                 .distinct()
