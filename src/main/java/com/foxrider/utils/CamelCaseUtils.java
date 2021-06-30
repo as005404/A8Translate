8df437b55a39e27232528b68731d7a4e4402d655
anton_isachenka <as005409@g.bstu.by>
2021-06-30 23:57:49 +0300

    [fix][test] add divider of camelCase and checker of CamelCase

---
+++ src/main/java/com/foxrider/utils/CamelCaseUtils.java
@@ -0,0 +1,35 @@
+package com.foxrider.utils;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.Locale;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class CamelCaseUtils {
+
+    private static final String camelAndPascalCasePattern = "^([a-z][a-z0-9]+|[A-Z][a-z0-9]+)((\\d)|([A-Z0-9][a-z0-9]+))*$";
+    private String selectedText;
+
+    public boolean isCamelCase() {
+        return selectedText.matches(camelAndPascalCasePattern);
+    }
+
+    public String divideCamelCaseIntoLowerCaseWords() {
+        if (!isCamelCase()) {
+            return selectedText;
+        }
+
+        StringBuilder builder = new StringBuilder(selectedText);
+        for (int i = 0; i < builder.length(); i++) {
+            if (Character.isUpperCase(builder.charAt(i)) && i != 0) {
+                builder.insert(i, " ");
+                i++;
+            }
+        }
+        return builder.toString().toLowerCase(Locale.ENGLISH);
+    }
+}
---
+++ src/test/java/com/foxrider/utils/CamelCaseUtilsTest.java
@@ -0,0 +1,51 @@
+package com.foxrider.utils;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertTrue;
+
+public class CamelCaseUtilsTest {
+
+    @Test
+    public void dividePascalCaseIntoLowerCaseWordsTest() {
+        CamelCaseUtils utils = new CamelCaseUtils("DivideCamelCaseIntoLowerCaseWords");
+        String s = utils.divideCamelCaseIntoLowerCaseWords();
+        System.out.println(s);
+    }
+
+    @Test
+    public void divideCamelCaseIntoLowerCaseWordsTest() {
+        CamelCaseUtils utils = new CamelCaseUtils("divideCamelCaseIntoLowerCaseWords");
+        String s = utils.divideCamelCaseIntoLowerCaseWords();
+        System.out.println(s);
+    }
+
+    @Test
+    public void isCamelCaseTest() {
+        CamelCaseUtils utils = new CamelCaseUtils("xmlHttpRequest");
+        assertTrue(utils.isCamelCase());
+        utils.setSelectedText("newCustomerId");
+        assertTrue(utils.isCamelCase());
+        utils.setSelectedText("innerStopwatch");
+        assertTrue(utils.isCamelCase());
+        utils.setSelectedText("supportsIpv6OnIos");
+        assertTrue(utils.isCamelCase());
+        utils.setSelectedText("youTubeImporter");
+        assertTrue(utils.isCamelCase());
+        utils.setSelectedText("youtubeImporter");
+        assertTrue(utils.isCamelCase());
+
+        utils.setSelectedText("XmlHttpRequest");
+        assertTrue(utils.isCamelCase());
+        utils.setSelectedText("NewCustomerId");
+        assertTrue(utils.isCamelCase());
+        utils.setSelectedText("InnerStopwatch");
+        assertTrue(utils.isCamelCase());
+        utils.setSelectedText("SupportsIpv6OnIos");
+        assertTrue(utils.isCamelCase());
+        utils.setSelectedText("YouTubeImporter");
+        assertTrue(utils.isCamelCase());
+        utils.setSelectedText("YoutubeImporter");
+        assertTrue(utils.isCamelCase());
+    }
+}
