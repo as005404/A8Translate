72bd7a889317e1128c793247cb2c7e37b2e7718d
anton_isachenka <as005409@g.bstu.by>
2021-07-01 18:41:50 +0300

    [fix] add check of emptiness of string to TranslationUtils

--- src/main/java/com/foxrider/utils/TranslationUtils.java
+++ src/main/java/com/foxrider/utils/TranslationUtils.java
@@ -1,5 +1,6 @@
 package com.foxrider.utils;
 
+import com.google.common.base.Strings;
 import com.intellij.openapi.editor.Editor;
 import com.intellij.openapi.ui.popup.Balloon;
 import com.intellij.openapi.ui.popup.JBPopupFactory;
@@ -9,8 +10,15 @@ import com.intellij.ui.JBColor;
 import java.awt.*;
 
 public class TranslationUtils {
-
+    private static final String MESSAGE_WHEN_RESULT_IS_EMPTY = "There is no translation for this text.\n Try to select a different part of the text.";
     public static void showPopupWindow(Editor editor, String result) {
+        if(Strings.isNullOrEmpty(result)){
+            buildBaloon(editor, MESSAGE_WHEN_RESULT_IS_EMPTY);
+        }
+        buildBaloon(editor, result);
+    }
+
+    private static void buildBaloon(Editor editor, String result) {
         JBPopupFactory.getInstance()
                 .createHtmlTextBalloonBuilder(result, null, new JBColor(new Color(255, 192, 203), Gray._68), null)
                 .setFadeoutTime(15000)
