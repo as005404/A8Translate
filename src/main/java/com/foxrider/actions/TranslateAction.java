0eb822533568c9d3a939c0386393e5746ed06ef9
anton_isachenka <as005409@g.bstu.by>
2021-07-02 18:03:53 +0300

    [fix] implement "todo: если язык с которого переводят является английским и язык на который переводят является английским (как когда target language русский и он переводит на англ),       //  то ничего не делать, потому что нет смысла переводить на какой-то другой общий язык кроме ангельского, починить баг с этим" and delete some boilerplate code

--- src/main/java/com/foxrider/actions/TranslateAction.java
+++ src/main/java/com/foxrider/actions/TranslateAction.java
@@ -11,13 +11,10 @@ import com.intellij.openapi.actionSystem.AnActionEvent;
 import com.intellij.openapi.actionSystem.PlatformDataKeys;
 import com.intellij.openapi.editor.Editor;
 import lombok.extern.slf4j.Slf4j;
-import org.apache.commons.lang.StringUtils;
 import org.jetbrains.annotations.NotNull;
 
 import java.util.Objects;
 
-// todo: если язык с которого переводят является английским и язык на который переводят является английским (как когда target language русский и он переводит на англ),
-//  то ничего не делать, потому что нет смысла переводить на какой-то другой общий язык кроме ангельского
 //todo: make listener on balloon when click on balloon with translation it will replace selected text
 @Slf4j
 public class TranslateAction extends AnAction {
@@ -25,16 +22,11 @@ public class TranslateAction extends AnAction {
     @Override
     public void actionPerformed(@NotNull AnActionEvent event) {
         Editor editor = event.getData(PlatformDataKeys.EDITOR);
-        if(editor == null){
+        if (editor == null) {
             log.info("TranslateAction. Editor is null");
             return;
         }
         String selectedText = Objects.requireNonNull(editor.getSelectionModel().getSelectedText());
-
-        if (StringUtils.isEmpty(selectedText)) {
-            log.info("TranslateAction. Selected text is empty");
-            return;
-        }
 
         selectedText = new CamelCaseUtils(selectedText.trim()).divideCamelCaseIntoLowerCaseWordsOrDefault();
 
@@ -47,14 +39,17 @@ public class TranslateAction extends AnAction {
             TranslationUtils.showPopupWindow(editor, "This language is not supported.\n" +
                     "List of supported languages: [en, ru, fr, pl, it, es, de]");
         } else if (detectedLanguage.equals(ContextReverseLanguage.valueFor(settings.targetLanguage))) {
+            // if language is not english and equal to our target language then translate selected text to english
+            if (!settings.targetLanguage.equalsIgnoreCase(ContextReverseLanguage.ENGLISH.detectedLang)) {
 
-            translatedText = new ContextReverseTranslator().getTranslationForText(
-                    ContextReverseLanguage.valueFor(settings.targetLanguage).lang,
-                    ContextReverseLanguage.ENGLISH.lang,
-                    selectedText
-            );
+                translatedText = new ContextReverseTranslator().getTranslationForText(
+                        ContextReverseLanguage.valueFor(settings.targetLanguage).lang,
+                        ContextReverseLanguage.ENGLISH.lang,
+                        selectedText
+                );
 
-            TranslationUtils.showPopupWindow(editor, translatedText);
+                TranslationUtils.showPopupWindow(editor, translatedText);
+            }
         } else {
             translatedText = new ContextReverseTranslator().getTranslationForText(
                     detectedLanguage.lang,
